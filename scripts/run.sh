af4d8c6debc2888102cd7d6023c7b183f90e8d64
Mark Watson <mark@turbulentresearch.com>
2026-07-12 12:41:35 -0300

    helper scripts

---
+++ scripts/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet run --project src/App/App.csproj -- --tap rawstack0
---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "pinging"
+ping 10.0.0.2 -c3
+echo " "
+
+sleep 3
+
+echo "curling"
+echo "curl -i http://10.0.0.2:8080/index.html"
+sleep 1
+curl -i http://10.0.0.2:8080/index.html
